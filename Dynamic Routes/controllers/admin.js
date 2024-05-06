484a418a8ddd2f7ae921e8c18c10e3328696c7a8
karthik-140 <shanigaramkarthik@gmail.com>
2024-05-06 16:02:23 +0530

    integrated with sequelize

--- Dynamic Routes/controllers/admin.js
+++ Dynamic Routes/controllers/admin.js
@@ -13,12 +13,17 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save()
-  .then(() => {
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description
+  })
+  .then(result => {
+    // console.log(result);
     res.redirect('/');
   })
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -27,13 +32,13 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId
-  Product.findById(prodId)
-  .then(([product]) => {
+  Product.findByPk(prodId)
+  .then((product) => {
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
-      product: product[0]
+      product: product
     });
   })
   .catch(err => console.log(err));
@@ -45,29 +50,59 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedPrice = req.body.price;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(productId, updatedTitle, updatedImageUrl, updatedDesc, updatedPrice);
-  updatedProduct.save()
+  // Product.findByPk(productId)
+  // .then(product => {
+  //   product.title = updatedTitle;
+  //   product.price = updatedPrice;
+  //   product.imageUrl = updatedImageUrl;
+  //   product.description = updatedDesc;
+  //   return product.save();
+  // })
+  // .then(() => {
+  //   res.redirect('/admin/products');
+  // })
+  // .catch(err => {
+  //   console.log(err);
+  // }) 
+  Product.update(
+    { title: updatedTitle, price: updatedPrice, imageUrl: updatedImageUrl, description: updatedDesc },
+    { where: {
+        id: productId
+      }
+    }
+  )
   .then(() => {
-    res.redirect('/admin/products');
-  })
-  .catch(err => console.log(err));
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-  .then(([products]) => {
+  Product.findAll()
+  .then((products) => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
   })
-  .catch(err => console.log(err));
+  .catch(err => {
+    console.log(err);
+  });
 };
 
 
 exports.getDeleteProduct = (req, res, next) => {
   const productId = req.params.productId;
-  Product.deleteProductById(productId);
-  res.redirect('/');
+  Product.destroy({
+    where: {
+      id: productId
+    }
+  })
+  .then((product) => {
+    res.redirect('/');
+  })
+  .catch(err => {
+    console.log(err);
+  })
 }
--- Dynamic Routes/util/database.js
+++ Dynamic Routes/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');  // npm install --save sequelize
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Root@123'
+const sequelize = new Sequelize('node-complete', 'root', 'Root@123', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
